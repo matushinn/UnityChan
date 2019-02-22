9d50745be35a9a7d428c85ad055c1ee682d73231
shotaro-ooe <shoooe97@gmail.com>
2019-02-23 00:48:12 +0900

    first commit

---
+++ Assets/UnityChan/Scripts/UnityChanMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanMove : MonoBehaviour {
+
+    Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float v;
+        v = Input.GetAxis("Vertical");
+        animator.SetFloat("speed", v);
+
+        if (Input.GetKeyDown("space"))
+        {
+            print("success");
+            animator.SetBool("jump", true);
+        }
+        else
+        {
+            animator.SetBool("jump", false);
+        }
+    }
+}
